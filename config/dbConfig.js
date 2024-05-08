0ffc9613164ff55cbcfad4ebfe16b30a3218a887
andgeral <geraldinebecerra93@gmail.com>
2024-05-07 22:55:43 -0400

    se cambian los require por import

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,11 +1,14 @@
-const { Pool } = require("pg");
+import { Pool } from "pg";
+import "dotenv/config";
+
+const { DB_PASSWORD, DB_USER, DB_DATABASE, DB_HOST } = process.env;
 
 const pool = new Pool({
-  user:DB_USER,
+  user: DB_USER,
   host: DB_HOST,
   database: DB_DATABASE,
   password: DB_PASSWORD,
-  port: 5432,
+  port: DB_PORT,
 });
 
 export default pool;
