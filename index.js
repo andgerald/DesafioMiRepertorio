8beee94e3b4c4269467882807cd6c8c61a97fef3
andgeral <geraldinebecerra93@gmail.com>
2024-05-08 00:16:02 -0400

    se agregan rutas para  crear y para listar todas las canciones

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import path from "path";
+import { agregar, todos } from "./consultas/consultas.js";
 
 const __dirname = path.resolve();
 const app = express();
@@ -14,4 +15,15 @@ app.listen(PORT, () => {
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/cancion", async (req, res) => {
+  const { titulo, artista, tono } = req.body;
+  const result = await agregar({ titulo, artista, tono });
+  res.json(result);
+});
+
+app.get("/canciones", async (req, res) => {
+  const result = await todos();
+  res.json(result);
 });
