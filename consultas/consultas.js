2a64fedf25a84dfbd822967730195710946cffc6
andgeral <geraldinebecerra93@gmail.com>
2024-05-08 00:41:36 -0400

    se agrega consulta para eliminar una cancion

--- consultas/consultas.js
+++ consultas/consultas.js
@@ -1,17 +1,24 @@
 import pool from "../config/dbConfig.js";
 
 //Funcion para agregar
-export const agregar = async ({ titulo, artista, tono }) => {
+export const agregar = async (titulo, artista, tono) => {
   const result = await pool.query(
     "INSERT INTO canciones (titulo,artista,tono) values($1,$2,$3) RETURNING *",
     [titulo, artista, tono]
   );
-  console.log(result);
-  console.log("Registro agregado con exito", result.rows);
+
   return result.rows[0];
 };
 
 export const todos = async () => {
   const result = await pool.query("SELECT * FROM canciones");
   return result.rows;
+};
+
+export const eliminar = async (id) => {
+  const result = await pool.query(
+    "DELETE FROM canciones WHERE id=$1 RETURNING *",
+    [id]
+  );
+  return result.rows[0];
 };
