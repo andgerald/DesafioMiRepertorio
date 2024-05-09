ae32517d1b8956a9d730f9b7d938b0e31b358c1d
andgeral <geraldinebecerra93@gmail.com>
2024-05-08 22:40:42 -0400

    se agrega archivo de errores

---
+++ error.js
@@ -0,0 +1,38 @@
+export const errors = (code, status, message) => {
+  switch (code) {
+    case "28P01":
+      status = 400;
+      message = "Usuario o clave incorrectos: ";
+      break;
+    case "42P01":
+      status = 400;
+      message = "No existe la tabla consultada ";
+      break;
+    case "3D000":
+      status = 400;
+      message = "La base de datos no existe";
+      break;
+    case "ENOTFOUND":
+      status = 500;
+      message = "Error en valor usado como localhost";
+      break;
+    case "ECONNREFUSED":
+      status = 500;
+      message = "Error en el puerto de conexion ";
+      break;
+    case "23502":
+      status = 400;
+      message = "Falta ingresar parametros en la consulta";
+      break;
+    case "42601":
+      status = 400;
+      message = "Error en la consulta";
+      break;
+    default:
+      status = 500;
+      message = "Error generico del Servidor";
+      break;
+  }
+
+  return { code, status, message };
+};
