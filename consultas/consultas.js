c5fb4f7b148334ee7514c584bf00833ade9dbfa3
andgeral <geraldinebecerra93@gmail.com>
2024-05-08 22:41:09 -0400

    se agregan errores

--- consultas/consultas.js
+++ consultas/consultas.js
@@ -1,32 +1,55 @@
 import pool from "../config/dbConfig.js";
+import { errors } from "../error.js";
 
-//Funcion para agregar
+let message = "";
+let status = "";
+
 export const agregar = async (titulo, artista, tono) => {
-  const result = await pool.query(
-    "INSERT INTO canciones (titulo,artista,tono) values($1,$2,$3) RETURNING *",
-    [titulo, artista, tono]
-  );
+  try {
+    const result = await pool.query(
+      "INSERT INTO canciones (titulo,artista,tono) values($1,$2,$3) RETURNING *",
+      [titulo, artista, tono]
+    );
 
-  return result.rows[0];
+    return result.rows[0];
+  } catch (e) {
+    const error = errors(e.code, status, message);
+    return e.code, error.status, error.message;
+  }
 };
 
 export const todos = async () => {
-  const result = await pool.query("SELECT * FROM canciones");
-  return result.rows;
+  try {
+    const result = await pool.query("SELECT * FROM canciones");
+    return result.rows;
+  } catch (e) {
+    const error = errors(e.code, status, message);
+    return e.code, error.status, error.message;
+  }
 };
 
 export const eliminar = async (id) => {
-  const result = await pool.query(
-    "DELETE FROM canciones WHERE id=$1 RETURNING *",
-    [id]
-  );
-  return result.rows[0];
+  try {
+    const result = await pool.query(
+      "DELETE FROM canciones WHERE id=$1 RETURNING *",
+      [id]
+    );
+    return result.rows[0];
+  } catch (e) {
+    const error = errors(e.code, status, message);
+    return e.code, error.status, error.message;
+  }
 };
 
 export const editar = async (titulo, artista, tono, id) => {
-  const result = await pool.query(
-    "UPDATE canciones  SET titulo = $1, artista = $2, tono = $3 WHERE id = $4  RETURNING *",
-    [titulo, artista, tono, id]
-  );
-  return result.rows[0];
+  try {
+    const result = await pool.query(
+      "UPDATE canciones  SET titulo = $1, artista = $2, tono = $3 WHERE id = $4  RETURNING *",
+      [titulo, artista, tono, id]
+    );
+    return result.rows[0];
+  } catch (e) {
+    const error = errors(e.code, status, message);
+    return e.code, error.status, error.message;
+  }
 };
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from "express";
 import "dotenv/config";
 import path from "path";
 import { agregar, todos, eliminar, editar } from "./consultas/consultas.js";
+import { errors } from "./error.js";
 
+let message = "";
 const __dirname = path.resolve();
 const app = express();
 const { PORT } = process.env;
@@ -18,31 +20,52 @@ app.get("/", (req, res) => {
 });
 
 app.post("/cancion", async (req, res) => {
-  const { titulo, artista, tono } = req.body;
-  const result = await agregar(titulo, artista, tono);
-  res.json(result);
+  try {
+    const { titulo, artista, tono } = req.body;
+    const result = await agregar(titulo, artista, tono);
+    res.json(result);
+  } catch (e) {
+    const error = errors(e.code, message);
+    return e.code, error.status, error.message;
+  }
 });
 
 app.get("/canciones", async (req, res) => {
-  const result = await todos();
-  res.json(result);
+  try {
+    const result = await todos();
+    res.json(result);
+  } catch (e) {
+    res.send(e.message);
+  }
 });
 
 app.delete("/cancion", async (req, res) => {
-  const { id } = req.query;
-  const result = await eliminar(id);
-  res.json(result);
+  try {
+    const { id } = req.query;
+    const result = await eliminar(id);
+    res.json(result);
+  } catch (e) {
+    res.send(e.message);
+  }
 });
 
 app.put("/cancion", async (req, res) => {
-  const { id } = req.query;
-  const result = await eliminar(id);
-  res.json(result);
+  try {
+    const { id } = req.query;
+    const result = await eliminar(id);
+    res.json(result);
+  } catch (e) {
+    res.send(e.message);
+  }
 });
 
 app.put("/cancion/:id", async (req, res) => {
-  const { titulo, artista, tono } = req.body;
-  const { id } = req.params;
-  const result = await editar(titulo, artista, tono, id);
-  res.json(result);
+  try {
+    const { titulo, artista, tono } = req.body;
+    const { id } = req.params;
+    const result = await editar(titulo, artista, tono, id);
+    res.json(result);
+  } catch (e) {
+    res.send(e.message);
+  }
 });
